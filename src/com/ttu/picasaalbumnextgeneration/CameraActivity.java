107ba4142ec9596edf1593a7f45a819eb14909e2
Tomi Tuhkanen <tomi.tuhkanen@iki.fi>
2013-03-17 23:55:00 +0200

    Activity for camera

---
+++ src/com/ttu/picasaalbumnextgeneration/CameraActivity.java
@@ -0,0 +1,59 @@
+package com.ttu.picasaalbumnextgeneration;
+
+import java.io.File;
+
+import android.app.Activity;
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+// http://stackoverflow.com/questions/2729267/android-camera-intent
+public class CameraActivity extends Activity {
+
+	private static int TAKE_PICTURE = 1;
+	private Uri imageUri;
+	
+	public void takePhoto(View view) {
+	    Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+	    File photo = new File(Environment.getExternalStorageDirectory(),  "Pic.jpg");
+	    intent.putExtra(MediaStore.EXTRA_OUTPUT,
+	            Uri.fromFile(photo));
+	    imageUri = Uri.fromFile(photo);
+	    startActivityForResult(intent, TAKE_PICTURE);
+	}
+	
+	@Override
+	public void onActivityResult(int requestCode, int resultCode, Intent data) {
+	    super.onActivityResult(requestCode, resultCode, data);
+	    switch (requestCode) {
+	    case 1:
+	        if (resultCode == Activity.RESULT_OK) {
+	            Uri selectedImage = imageUri;
+	            getContentResolver().notifyChange(selectedImage, null);
+	            // TODO: Create imageview
+	            //ImageView imageView = (ImageView) findViewById(R.id.ImageView);
+	            ContentResolver cr = getContentResolver();
+	            Bitmap bitmap;
+	            try {
+	                 bitmap = android.provider.MediaStore.Images.Media
+	                 .getBitmap(cr, selectedImage);
+
+	                //imageView.setImageBitmap(bitmap);
+	                Toast.makeText(this, selectedImage.toString(),
+	                        Toast.LENGTH_LONG).show();
+	            } catch (Exception e) {
+	                Toast.makeText(this, "Failed to load", Toast.LENGTH_SHORT)
+	                        .show();
+	                Log.e("Camera", e.toString());
+	            }
+	        }
+	    }
+	}
+}
