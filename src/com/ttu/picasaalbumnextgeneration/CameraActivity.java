5b4db9d8190d339b743e77c48e5d633d27b7e64c
Tomi Tuhkanen <tomi.tuhkanen@iki.fi>
2013-03-21 00:43:32 +0200

    Added filename for camera. User permissions.

--- src/com/ttu/picasaalbumnextgeneration/CameraActivity.java
+++ src/com/ttu/picasaalbumnextgeneration/CameraActivity.java
@@ -1,6 +1,8 @@
 package com.ttu.picasaalbumnextgeneration;
 
 import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import android.app.Activity;
 import android.content.ContentResolver;
@@ -11,40 +13,61 @@ import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.util.Log;
-import android.view.View;
 import android.widget.ImageView;
 import android.widget.Toast;
 
 // http://stackoverflow.com/questions/2729267/android-camera-intent
 public class CameraActivity extends Activity {
 
+	private static final String JPEG_FILE_PREFIX = "Img_";
+	private static final String JPEG_FILE_SUFFIX = "JPG";
 	private static int TAKE_PICTURE = 1;
 	private Uri imageUri;
 	
 	public void takePhoto() {
 	    Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
-	    //File photo = new File(Environment.getExternalStorageDirectory(),  "Pic.jpg");
-	    //intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(photo));
-	    //imageUri = Uri.fromFile(photo);
+	    // Save photo from intent to this File
+	    File photo = new File(getAlbumDir(),  getImageName());
+	    intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(photo));
+	    imageUri = Uri.fromFile(photo);
 	    startActivityForResult(intent, TAKE_PICTURE);
+	}
+	
+	private String getImageName() {
+	    // Create an image file name
+	    String timeStamp = 
+	        new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+	    String imageFileName = JPEG_FILE_PREFIX + timeStamp;
+	
+	    return imageFileName + "." + JPEG_FILE_SUFFIX;
+	}
+	
+	private String getAlbumDir(){
+		return Environment.getExternalStorageDirectory() + File.separator + "MYAPP";		
 	}
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_camera);
+		
+		if (Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED) == false)
+		{
+			 Toast.makeText(this, "Storage not mounted", Toast.LENGTH_SHORT).show();
+		}
+		
 		takePhoto();
 	}
 	
 	@Override
 	public void onActivityResult(int requestCode, int resultCode, Intent data) {
 	    super.onActivityResult(requestCode, resultCode, data);
+	   
 	    switch (requestCode) {
 	    case 1:
 	        if (resultCode == Activity.RESULT_OK) {
 	            Uri selectedImage = imageUri;
 	            getContentResolver().notifyChange(selectedImage, null);
-	            // TODO: Create imageview
 	            ImageView imageView = (ImageView) findViewById(R.id.ImageView);
 	            ContentResolver cr = getContentResolver();
 	            Bitmap bitmap;
